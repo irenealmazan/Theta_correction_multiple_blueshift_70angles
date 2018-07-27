87e6c15683c978178aed7e9fda2e72cbeb938cf6
irenealmazan <irenealmazan@hotmail.com>
2018-07-27 17:00:27 -0500

    work only in one single percentage

--- multiple_ERHIO_init.m
+++ multiple_ERHIO_init.m
@@ -2,48 +2,49 @@ addpath(genpath('./m_scripts/'));
 addpath(genpath('./calc_functions'));
 
 
-jitterlevel = [0 5 10 20 40];%0 10 20 40];
-mncrate_array = [2e2 2e2 1e3];
-noiseflag_array = [0 1 1];
-noiselevel_array = [1 2 3]
+jitterlevel_1 = [20];%[0 5 10 20 40];
+mncrate_array_2 = [1e3];
+noiseflag_array_2 = [1];
+noiselevel_array_2 = [3];
 %%%% no noise
 
 
 
-for mm = 1:numel(mncrate_array)
+for mm = 1:numel(mncrate_array_2)
     
-    noiseflag = noiseflag_array(mm);
+    noiseflag = noiseflag_array_2(mm);
     if(noiseflag)
         display('ADDING NOISE')
     else
         display('NO NOISE')
     end
     
-    mncntrate = mncrate_array(mm);
+    mncntrate = mncrate_array_2(mm);
     
-    noiselevel_str = num2str(noiselevel_array(mm));
+    noiselevel_str = num2str(noiselevel_array_2(mm));
     
-    for jjj = 1:numel(jitterlevel)
+    for jjj = 1:numel(jitterlevel_1)
         
-        percent = jitterlevel(jjj);
+        percent = jitterlevel_1(jjj);
        
         load(['data_initial_alljitter/data_cnt_' noiselevel_str '_jitter_' num2str(percent)]);
 
         min_chi = 1;
         for kk = 1:5
+            display(num2str(kk))
             ER_HIO;
            struct_ER_HIO(kk).chi = newobj.chi;     
            struct_ER_HIO(kk).dp = newobj.dp;
             struct_ER_HIO(kk).support_new = support_new;
             
-           if min_chi <newobj.chi(end)
+           if min_chi >newobj.chi(end)
                min_kk = kk;
                min_chi = newobj.chi(end);
            end
            
         end
         
-        struct_best_ERHIO = struct_ERHIO(min_kk);
+        struct_best_ERHIO = struct_ER_HIO(min_kk);
         
         save(['data_ERHIO/struct_ERHIO_ini' noiselevel_str '_jitter_' num2str(percent)],'struct_best_ERHIO');
         
